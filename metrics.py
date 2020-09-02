1d10e8fa397cc253e57f877c60950554e3bd4719
Kushagra Raina <kushagra.raina@gmail.com>
2020-09-03 02:38:15 +0530

    model, metric update

--- metrics.py
+++ metrics.py
@@ -8,40 +8,73 @@ from matplotlib import pyplot
 import numpy as np
 import os
 
-path = r'./results/results50.csv'
-tp = 0
-fp = 0
-tn = 0
-fn = 0
-df = pd.read_csv(path)
+path = r'./ft_results30.csv'
 
-test_y = []
-lr_probs = []
-for index, row in df.iterrows():
-    if row['Predictions'] == 1 and row['Label'] == 1:
-        tp += 1
-    elif row['Predictions'] == 1 and row['Label'] == 0:
-        fp += 1
-    elif row['Predictions'] == 0 and row['Label'] == 1:
-        fn += 1
-    else:
-        tn += 1
-    test_y.append(row['Label'])
-    lr_probs.append(row['Predictions'])
+def metric(path):
+    df = pd.read_csv(path)
+    tp = 0
+    fp = 0
+    tn = 0
+    fn = 0
+    test_y = []
+    lr_probs = []
+    for index, row in df.iterrows():
+        if row['Predictions'] == 1 and row['Label'] == 1:
+            tp += 1
+        elif row['Predictions'] == 1 and row['Label'] == 0:
+            fp += 1
+        elif row['Predictions'] == 0 and row['Label'] == 1:
+            fn += 1
+        else:
+            tn += 1
+        test_y.append(row['Label'])
+        lr_probs.append(row['Predictions'])
 
-lr_probs = np.array(lr_probs)
-lr_auc = roc_auc_score(test_y, lr_probs)
-print('Logistic: ROC AUC=%.3f' % lr_auc)
-lr_fpr, lr_tpr, _ = roc_curve(test_y, lr_probs)
-pyplot.plot(lr_fpr, lr_tpr, marker='.', label='Logistic')
-pyplot.xlabel('False Positive Rate')
-pyplot.ylabel('True Positive Rate')
+    print(len(test_y), len(lr_probs))
+    lr_probs = np.array(lr_probs)
+    lr_auc = roc_auc_score(test_y, lr_probs)
+    print('Logistic: ROC AUC=%.3f' % lr_auc)
+    lr_fpr, lr_tpr, _ = roc_curve(test_y, lr_probs)
+    pyplot.plot(lr_fpr, lr_tpr, marker='.', label='Logistic')
+    pyplot.xlabel('False Positive Rate')
+    pyplot.ylabel('True Positive Rate')
 
-pyplot.legend()
-pyplot.show()
-# print(tp,fp,fn,tn)
+    pyplot.legend()
+    pyplot.show()
+    # print(tp,fp,fn,tn)
 
-specificity = tn / (fp + tn)
-sensitivity = tp / (tp + fn)
-print(specificity, sensitivity)
+    specificity = tn / (fp + tn)
+    sensitivity = tp / (tp + fn)
+    print(specificity, sensitivity)
 
+metric(path)
+
+
+# AUC for a binary classifier
+# def auc(y_true, y_pred):
+#     ptas = tf.stack([binary_PTA(y_true, y_pred, k) for k in np.linspace(0, 1, 1000)], axis=0)
+#     pfas = tf.stack([binary_PFA(y_true, y_pred, k) for k in np.linspace(0, 1, 1000)], axis=0)
+#     pfas = tf.concat([tf.ones((1,)), pfas], axis=0)
+#     binSizes = -(pfas[1:]-pfas[:-1])
+#     s = ptas*binSizes
+#     return K.sum(s, axis=0)
+
+
+# PFA, prob false alert for binary classifier
+# def binary_PFA(y_true, y_pred, threshold=K.variable(value=0.5)):
+#     y_pred = K.cast(y_pred >= threshold, 'float32')
+#     # N = total number of negative labels
+#     N = K.sum(1 - y_true)
+#     # FP = total number of false alerts, alerts from the negative class labels
+#     FP = K.sum(y_pred - y_pred * y_true)
+#     return FP/N
+
+
+# P_TA prob true alerts for binary classifier
+# def binary_PTA(y_true, y_pred, threshold=K.variable(value=0.5)):
+#     y_pred = K.cast(y_pred >= threshold, 'float32')
+#     # P = total number of positive labels
+#     P = K.sum(y_true)
+#     # TP = total number of correct alerts, alerts from the positive class labels
+#     TP = K.sum(y_pred * y_true)
+#     return TP/P
--- model.py
+++ model.py
@@ -1,86 +1,207 @@
 import keras as K
-import os
+import tensorflow as tf
+from os import path
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D, MaxPool2D, Flatten
+from keras.layers import Dense, Conv2D, MaxPool2D, Flatten, Dropout
 from keras.preprocessing.image import ImageDataGenerator
-from keras.optimizers import Adam
+from keras.optimizers import Adam, SGD
 from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.utils.np_utils import to_categorical
+import matplotlib.pyplot as plt
+from keras.callbacks import Callback
+from sklearn.metrics import roc_auc_score
 import numpy as np
+import sys
+import math
 
-tr_data = ImageDataGenerator(rescale=1. / 255)
-batchsize = 4
-train_data_generator = tr_data.flow_from_directory(directory='./finalDataset/train',
-                                                   target_size=(200, 135),
-                                                   batch_size=batchsize,
-                                                   class_mode=None,
-                                                   shuffle=True)
+batch_size = 4
+num_classes = 2
+num_epochs = 0
+train_in = './finalDataset/train'
+test_in = './finalDataset/val'
 
-ts_data = ImageDataGenerator(rescale=1. / 255)
-test_data_generator = ts_data.flow_from_directory(directory='./finalDataset/val',
-                                                  target_size=(200, 135),
-                                                  batch_size=batchsize,
-                                                  class_mode=None,
-                                                  shuffle=True)
 
-num_classes = len(train_data_generator.class_indices)
+# def auc_roc(generator):
+#     # any tensorflow metric
+#     value, update_op = tf.contrib.metrics.streaming_auc(y_pred, y_true)
+#
+#     # find all variables created for this metric
+#     metric_vars = [i for i in tf.local_variables() if 'auc_roc' in i.name.split('/')[1]]
+#
+#     # Add metric variables to GLOBAL_VARIABLES collection.
+#     # They will be initialized for new session.
+#     for v in metric_vars:
+#         tf.add_to_collection(tf.GraphKeys.GLOBAL_VARIABLES, v)
+#
+#     # force to update metric values
+#     with tf.control_dependencies([update_op]):
+#         value = tf.identity(value)
+#         return value
 
-model = Sequential()
 
-model.add(Conv2D(input_shape=(200, 135, 3), filters=64, kernel_size=(3, 3), padding="same", activation="relu"))
-model.add(Conv2D(filters=64, kernel_size=(3, 3), padding="same", activation="relu"))
-model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+class RocCallback(Callback):
+    def __init__(self, validation_data):
+        # self.x = training_data[0]
+        # self.y = training_data.classes
+        self.x_val = validation_data
+        self.y_val = to_categorical(validation_data.classes, num_classes=2)
 
-model.add(Conv2D(filters=128, kernel_size=(3, 3), padding="same", activation="relu"))
-model.add(Conv2D(filters=128, kernel_size=(3, 3), padding="same", activation="relu"))
-model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
 
-model.add(Conv2D(filters=256, kernel_size=(3, 3), padding="same", activation="relu"))
-model.add(Conv2D(filters=256, kernel_size=(3, 3), padding="same", activation="relu"))
-model.add(Conv2D(filters=256, kernel_size=(3, 3), padding="same", activation="relu"))
-model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+    def on_train_begin(self, logs={}):
+        return
 
-model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
-model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
-model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
-model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+    def on_train_end(self, logs={}):
+        return
 
-model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
-model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
-model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
-model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+    def on_epoch_begin(self, epoch, logs={}):
+        return
 
-model.add(Flatten())
-model.add(Dense(units=4096, activation="relu"))
-model.add(Dense(units=4096, activation="relu"))
-model.add(Dense(units=1, activation="sigmoid"))
+    def on_epoch_end(self, epoch, logs={}):
+        # y_pred_train = self.model.predict_proba(self.x)
+        # roc_train = roc_auc_score(self.y, y_pred_train)
+        # y_pred_val = self.model.predict_proba(self.x_val)
+        y_pred_val = self.model.predict_generator(self.x_val, steps=len(self.x_val.filenames)//batch_size)
+        roc_val = roc_auc_score(self.y_val, y_pred_val)
+        # print('\rroc-auc_train: %s - roc-auc_val: %s' % (str(round(roc_train,4)),str(round(roc_val,4))),end=100*' '+'\n')
+        print('\rroc-auc_val: %s'%(str(round(roc_val, 4))), end=100*' '+'\n')
+        return
 
-opt = Adam(lr=0.001)
-model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+    def on_batch_begin(self, batch, logs={}):
+        return
+
+    def on_batch_end(self, batch, logs={}):
+        return
+
+def setup_generators(train_in, test_in):
+    tr_data = ImageDataGenerator(rescale=1. / 255)
+    train_data_generator = tr_data.flow_from_directory(directory=train_in,
+                                                       target_size=(175, 175),
+                                                       batch_size=batch_size,
+                                                       class_mode='categorical',
+                                                       shuffle=True)
+
+    ts_data = ImageDataGenerator(rescale=1. / 255)
+    test_data_generator = ts_data.flow_from_directory(directory=test_in,
+                                                      target_size=(175, 175),
+                                                      batch_size=batch_size,
+                                                      class_mode='categorical',
+                                                      shuffle=True)
+    return train_data_generator, test_data_generator
+
+# np.save('./cropped_features_train.npy', )
+
+
+# gen = test_data_generator.next()
+# print(gen)
+# print(train_data_generator.filenames)
+# sys.exit(0)
+
+# num_classes = len(train_data_generator.class_indices)
+# print(train_data_generator.filenames)
+# nb_train_samples=len(train_data_generator)
+# print(nb_train_samples)
+
+
+# train_labels = to_categorical(train_data_generator.classes, num_classes=num_classes)
+# validation_labels = to_categorical(test_data_generator.classes, num_classes=num_classes)
+# print(train_labels)
+
+def def_model():
+    model = Sequential()
+
+    model.add(Conv2D(input_shape=(175, 175, 3), filters=64, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=64, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+
+    model.add(Conv2D(filters=128, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=128, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+
+    model.add(Conv2D(filters=256, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=256, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=256, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+
+    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+
+    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+
+    model.add(Flatten())
+    model.add(Dense(units=4096, activation="relu"))
+    model.add(Dropout(0.5))
+    model.add(Dense(units=4096, activation="relu"))
+    model.add(Dense(units=2, activation="sigmoid"))
+
+    opt = Adam(lr=0.001)
+    model.compile(optimizer=SGD(lr=0.0001, momentum=0.9),
+                  loss='binary_crossentropy',
+                  metrics=['acc'])
+    return model
 
 # keras.losses.categorical_crossentropy for multiclass!
 
+
+model = def_model()
+
 model.summary()
-# model.load_weights('model_vgg16.h5')
-checkpoint = ModelCheckpoint("vgg16_1.h5",
+
+
+train_data_generator,test_data_generator = setup_generators(train_in, test_in)
+
+model_weights = './untrained_vgg16.h5'
+if path.exists(model_weights):
+    model.load_weights(model_weights)
+
+checkpoint = ModelCheckpoint("utvgg_norm.h5",
                              monitor='val_acc',
                              verbose=1,
                              save_best_only=True,
                              save_weights_only=False,
                              mode='auto',
                              period=1)
+# quit()
+# early = EarlyStopping(monitor='val_acc',
+#                       min_delta=0,
+#                       patience=20,
+#                       verbose=1,
+#                       mode='auto')
 
-early = EarlyStopping(monitor='val_acc', min_delta=0, patience=20, verbose=1, mode='auto')
+history = model.fit_generator(steps_per_epoch=len(train_data_generator.filenames) // batch_size,
+                              generator=train_data_generator,
+                              validation_data=test_data_generator,
+                              validation_steps=10,
+                              epochs=num_epochs,
+                              callbacks=[checkpoint, RocCallback(test_data_generator)])
 
-hist = model.fit_generator(steps_per_epoch=len(train_data_generator.filenames) / batchsize,
-                           generator=train_data_generator,
-                           validation_data=test_data_generator,
-                           validation_steps=10,
-                           epochs=10,
-                           callbacks=[checkpoint, early])
+# acc = history.history['acc']
+# val_acc = history.history['val_acc']
+# loss = history.history['loss']
+# val_loss = history.history['val_loss']
+#
+# epochs = range(len(acc))
+#
+# plt.plot(epochs, acc, 'r')
+# plt.plot(epochs, acc, 'r')
+# plt.title('Training and validation accuracy')
+# plt.figure()
+#
+# plt.plot(epochs, loss, 'r')
+# plt.plot(epochs, val_loss, 'r')
 
-model.save_weights('model_vgg16.h5')
+model.save_weights('utvgg_norm.h5.h5')
 
 # print(len(test_data_generator.filenames))
 # print(test_data_generator.filenames)
-prediction = model.predict_generator(test_data_generator, steps=len(test_data_generator.filenames))
-print(prediction)
+prediction = model.predict_generator(test_data_generator,
+                                     steps=len(test_data_generator.filenames))
+# print(prediction)
+# np.save('predictions.txt', prediction)
+
+
+
